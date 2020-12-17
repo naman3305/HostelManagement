d2d81203e772ca268bc4acea702afdd9dcf150c9
naman3305 <59595534+naman3305@users.noreply.github.com>
2020-12-17 17:55:10 +0530

    Update conn.java

--- src/hostelmanagement/conn.java
+++ src/hostelmanagement/conn.java
@@ -1,13 +1,14 @@
 
 import java.sql.*;
 
-public class conn {
+public class conn
+{
      Connection c;
     Statement s;
     public conn(){  
         try{  
             Class.forName("com.mysql.cj.jdbc.Driver");  
-            c =DriverManager.getConnection("jdbc:mysql://localhost:3306/hostel","root","Johnsnow"); 
+            c =DriverManager.getConnection("jdbc:mysql://localhost:3306/hostel","root",""); 
             s =c.createStatement();  
         }catch(Exception e){ 
             System.out.println(e);
