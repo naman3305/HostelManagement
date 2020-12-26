3b6a026a8eee49864fde93813bca9b394891c298
naman3305 <59595534+naman3305@users.noreply.github.com>
2020-12-26 21:36:14 +0530

    Update HomePage.java

--- src/hostelmanagement/HomePage.java
+++ src/hostelmanagement/HomePage.java
@@ -4,7 +4,8 @@ import java.awt.event.*;
 import javax.swing.*;
 
 
-public class HomePage extends JFrame implements ActionListener{
+public class HomePage extends JFrame implements ActionListener
+{
     JButton b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13;
    HomePage()
    {
